98d824d474bead5830d2032b34fe740a8e012381
AJ <86043065+WonjaeJi0801@users.noreply.github.com>
2022-02-11 10:54:44 +0900

    Add basic routing in App.js

--- luca-client/src/App.js
+++ luca-client/src/App.js
@@ -1,16 +1,57 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Mainpage from "./pages/main";
-import Mypage from "./pages/mypage";
+import About from "./pages/about";
+import Main from "./pages/main";
 import Signup from "./pages/signup";
+import Mypage from "./pages/mypage";
+import ChangePassword from "./pages/changePassword";
+import Project from "./pages/project";
+
+import styled from "styled-components";
+import { useState } from "react";
+
+const Navigator = styled.div`
+  width: 100vw;
+  height: 10vh;
+  background-color: cyan;
+  text-align: center;
+`;
+
+const Backdrop = styled.div`
+  width: 100vw;
+  min-height: 90vh;
+  height: auto;
+  background-color: grey;
+  justify-content: center;
+  display: flex;
+`;
+
+const Container = styled.div`
+  min-height: 90vh;
+  height: auto;
+  background-color: lightyellow;
+  flex: 1 0 auto;
+  max-width: 1320px;
+  margin: auto;
+`;
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Mainpage />} />
-        <Route path="/mypage" element={<Mypage />} />
-      </Routes>
+    <div>
+      <Navigator>this is Navigator</Navigator>
+      <Backdrop>
+        <Container>
+          <Routes>
+            <Route path="/" element={isLogin ? <Main /> : <About />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/mypage" element={<Mypage />} />
+            <Route path="/changepassword" element={<ChangePassword />} />
+            <Route path="/project" element={<Project />} />
+          </Routes>
+        </Container>
+      </Backdrop>
     </div>
   );
 }
--- luca-client/src/pages/signup.js
+++ luca-client/src/pages/signup.js
@@ -1,11 +1,3 @@
-import Navbar from "../components/navbar";
-import Container from "../components/container";
-
 export default function Signup() {
-  return (
-    <div>
-      <Navbar />
-      <Container />
-    </div>
-  );
+  return <div>signup</div>;
 }
