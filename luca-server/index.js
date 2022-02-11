f4ce04c47f81312ad23575bfd4111ea7633ccfb7
QuatoHub <hyeonjun5256@gmail.com>
2022-02-11 15:50:39 +0900

    Add deploy

--- luca-server/index.js
+++ luca-server/index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
 
 const app = express();
 const port = 80;
 
-app.use(express.json());
+app.use(express.json()); 햣
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
 
 // test용
 app.get('/', (req, res) => {
