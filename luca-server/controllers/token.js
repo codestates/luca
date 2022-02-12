c1a23f63df16f38ad9bc9bed5ee93d02a5375a0d
QuatoHub <hyeonjun5256@gmail.com>
2022-02-12 16:55:42 +0900

    Check bcrypt module

---
+++ luca-server/controllers/token.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const { sign, verify } = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+module.exports = {
+    hashedpassword: (password) => {
+        const salt = bcrypt.genSaltSync();
+        return bcrypt.hashSync(password, salt);
+    },
+    comparepassword: (inputPWD, hash) => {
+        return bcrypt.compareSync(inputPWD, hash); //true 또는 false를 리턴
+    },
+    generateAccessToken: (data) => {
+        return sign({ data: data }, process.env.ACCESS_SECRET, {
+            expiresIn: 60 * 60 * 1000,
+        });
+    },
+    sendAccessToken: (res, data, accessToken) => {
+        res.cookie('accessToken', accessToken, {
+            path: '/',
+            maxAge: 24 * 60 * 60 * 1000,
+            sameSite: 'none',
+            secure: true,
+            httpOnly: true,
+        });
+        res.json({ data, message: 'OK' });
+    },
+    isAuthorized: (req) => {
+        const accessToken = req.cookies.accessToken;
+        if (!accessToken) {
+            return null;
+        }
+        try {
+            return verify(accessToken, process.env.ACCESS_SECRET);
+        } catch (err) {
+            //return null if invalid token
+            return null;
+        }
+    },
+};
