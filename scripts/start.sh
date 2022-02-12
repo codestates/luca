7bf65d79fd048fbf767d1cc31562c33af72a7f1c
QuatoHub <hyeonjun5256@gmail.com>
2022-02-12 18:06:06 +0900

    Add .env SERVER_PORT

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,5 +5,6 @@ export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DA
 export DATABASE_USER=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
+export SERVER_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
 
 authbind --deep pm2 start index.js
