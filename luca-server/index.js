65989fc4a39e10db4166a044fd678f6d760621c4
QuatoHub <hyeonjun5256@gmail.com>
2022-02-12 12:31:20 +0900

    Fix index.js

--- luca-server/index.js
+++ luca-server/index.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.SERVER_PORT;
+const port = 80;
 
 // Routes
 const indexRouter = require('./routes');
