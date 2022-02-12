1aa91471b84c389005c32193a4bf85b2bde38408
QuatoHub <hyeonjun5256@gmail.com>
2022-02-12 14:57:14 +0900

    Fix config.js

---
+++ luca-server/config/config.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const config = {
+  development: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: 'luca_database',
+    host: process.env.DATABASE_HOST,
+    dialect: 'mysql',
+    port: process.env.DATABASE_PORT,
+  },
+  production: {
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: 'luca_database',
+    host: process.env.DATABASE_HOST,
+    dialect: 'mysql',
+    port: process.env.DATABASE_PORT,
+  },
+};
+module.exports = config;
+
+
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /home/ubuntu/luca/luca-server
 
-# export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
-# export DATABASE_USER=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
-# export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
-# export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_USER=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
 # export SERVER_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PORT --query Parameters[0].Value | sed 's/"//g')
 
 authbind --deep pm2 start index.js
