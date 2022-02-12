9b8fde4d81e22b3743c6fb95de39a613653d6844
portis082 <portis082@gmail.com>
2022-02-12 17:07:34 +0900

    Add merge

--- luca-server/index.js
+++ luca-server/index.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
 const express = require("express");
+<<<<<<< HEAD
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const morgan = require('morgan');
 const cors = require('cors');
 // const bcrypt = require("bcrypt");
+=======
+// const cookieParser = require('cookie-parser');
+// const session = require('express-session');
+// const morgan = require('morgan');
+const cors = require('cors');
+>>>>>>> ca8b7177e2828e43301703f144e069ce05c8ab84
 
 const app = express();
 const port = process.env.PORT;
@@ -16,7 +23,11 @@ const indexRouter = require('./routes');
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+<<<<<<< HEAD
 app.use(cookieParser());
+=======
+// app.use(cookieParser());
+>>>>>>> ca8b7177e2828e43301703f144e069ce05c8ab84
 app.use(
   cors({
     origin: true,
@@ -24,12 +35,21 @@ app.use(
     credentials: true,
   }),
 );
+<<<<<<< HEAD
 app.use(session({
   secret: 'ras',
   resave: true,
   secure: false,
   saveUninitialized: false,
 }));
+=======
+// app.use(session({
+//   secret: 'ras',
+//   resave: true,
+//   secure: false,
+//   saveUninitialized: false,
+// }));
+>>>>>>> ca8b7177e2828e43301703f144e069ce05c8ab84
 
 // router
 app.use('/', indexRouter);
