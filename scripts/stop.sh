90ff9887364c7c66d40a3967fb0250e748ecebf9
QuatoHub <hyeonjun5256@gmail.com>
2022-02-12 16:49:12 +0900

    Add MVC model

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/luca/luca-server
 pm2 stop index.js 2> /dev/null || true
-<<<<<<< HEAD
 pm2 delete index.js 2> /dev/null || true
-=======
-pm2 delete index.js 2> /dev/null || true
->>>>>>> 53496c735aa0e8a6b5e32e30d6b734ef246ade36
