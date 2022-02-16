ffe9fe11ce3fec7b3986c141895503fa9463f993
portis082 <portis082@gmail.com>
2022-02-16 18:26:06 +0900

    Update index.js, server.js, socket.js

--- luca-server/socket.js
+++ luca-server/socket.js
@@ -1,106 +1,29 @@
-const arrayList = [
-    {
-        id:	0,
-        projectId: 1,
-        userId: 1,
-        content: 'mother',
-        parent: 0,
-        storage: 'mindmap'
-    },
-    {
-        id:	1,
-        projectId: 1,
-        userId: 1,
-        content: 'root',
-        parent: 1,
-        storage: 'mindmap'
-    },
-    {
-        id: 3,
-        projectId: 1,
-        userId: 1,
-        content: 'path1',
-        parent: 2,
-        storage: 'mindmap'
-    },
-    {
-        id:	4,
-        projectId: 1,
-        userId: 1,
-        content: 'path1-1',
-        parent: 3,
-        storage: 'mindmap'
-    },
-    {
-        id:	5,
-        projectId: 1,
-        userId: 1,
-        content: 'path1-2',
-        parent: 3,
-        storage: 'mindmap'
-    },
-    {
-        id:	6,
-        projectId: 1,
-        userId: 1,
-        content: 'path1-2-1',
-        parent: 5,
-        storage: 'mindmap'
-    },
-    {
-        id:	7,
-        projectId: 1,
-        userId: 1,
-        content: 'path2',
-        parent: 2,
-        storage: 'mindmap'
-    },
-    {
-        id:	8,
-        projectId: 1,
-        userId: 1,
-        content: 'path3',
-        parent: 1,
-        storage: 'mindmap'
-    },
-    {
-        id:	9,
-        projectId: 1,
-        userId: 1,
-        content: 'path3-1',
-        parent: 8,
-        storage: 'mindmap'
-    },
-]
+const app = require('./index')
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, { cors: {origin: "*"}});
+const project = io.of('/project1')
 
-let treeModel = function (arrayList, rootId) {
-    let rootNodes = [];
-    let traverse = function (nodes, card, index) {
-        if (nodes instanceof Array) {
-            return nodes.some(function (node) {
-                if (node.id === card.parent) {
-                    node.children = node.children || [];
-                    return node.children.push(arrayList.splice(index, 1)[0]);
-                }
+project.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.emit("conSuccess", 'success');
 
-                return traverse(node.children, card, index);
-            });
-        }
-    };
+  console.log(socket.nsp)
 
-    while (arrayList.length > 0) {
-        arrayList.some(function (card, index) {
-            console.log(card, index)
-            if (card.parent === rootId) {
-                return rootNodes.push(arrayList.splice(index, 1)[0]);
-            }
+  socket.on("add", (data) =>{
+    console.log(socket.nsp)
+    console.log('==add==', socket.id);
+    console.log(data);
+    socket.emit("addReply", {'Tree': 'tree1'});
+    socket.broadcast.emit("addReply", {'Tree': 'tree1'});
+  });
 
-            return traverse(rootNodes, card, index);
-        });
-    }
+  socket.on("delete", (data) =>{
+    console.log(socket.nsp)
+    console.log('==delete==');
+    console.log(data);
+    socket.emit("deleteReply", { 'Tree': 'tree2'});
+    socket.broadcast.emit("deleteReply", {'Tree': 'tree2'});
+  });
+});
 
-    return rootNodes;
-};
-
-const result = treeModel(arrayList, 0);
-console.log(result)
+module.exports = server;
