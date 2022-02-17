3bb04945077997b729094bacd236617ae4079d36
flowervillagearp <iyunseong57@gmail.com>
2022-02-17 15:32:40 +0900

    Fix changepwpage flexbox

--- luca-client/src/index.js
+++ luca-client/src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { store } from './redux/store.js';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <BrowserRouter>
   {/* <React.StrictMode> */}
+  {/* <Provider store={store}> */}
     <App />
+  {/* </Provider> */}
   {/* </React.StrictMode> */}
   </BrowserRouter>,
   document.getElementById('root')
