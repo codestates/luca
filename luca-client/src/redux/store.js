93e9d33a174719210e5b773bc75b531fa81b4593
portis082 <portis082@gmail.com>
2022-02-19 16:32:56 +0900

    Update project list component

--- luca-client/src/redux/store.js
+++ luca-client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "./slicer/loginSlice";
 import userInfoReducer from "./slicer/userInfoSlice";
+import counterSliceRuducer from "./counterslice"
 
 export default configureStore({
   reducer: {
     login: loginReducer,
     userInfo: userInfoReducer,
+    counterSlice: counterSliceRuducer
   },
 });
