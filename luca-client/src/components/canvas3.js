2ea525363aa350970f7a44307f4799cdf55d05fe
AJ <86043065+WonjaeJi0801@users.noreply.github.com>
2022-03-04 20:26:10 +0900

    Add style in minmap

--- luca-client/src/components/canvas3.js
+++ luca-client/src/components/canvas3.js
@@ -10,13 +10,14 @@ import Finder from "./finder";
 import Timer from "./timer";
 
 // const Container = styled.div`
-//   width: 99vw;
-//   height: 99vh;
+//   width: 200vw;
+//   height: 200vh;
 //   border: solid red 1px;
 //   overflow: visible;
 // `;
 
 const Exit = styled.div`
+  position: fixed;
   > button {
     flex: 1 0 auto;
     margin: 4px 4px 0 4px;
@@ -127,45 +128,53 @@ const Rootbox = styled.div`
     return String(props.coordX) + "px";
   }};
   transform: translate(-50%, -50%);
-  background-color: white;
+  background-color: rgb(70, 70, 70);
   text-align: center;
-  line-height: 0.9em;
+  line-height: 1.2em;
   inline-size: 8em;
   border-radius: 8em;
+  border: solid grey 2px;
   box-shadow: ${(props) =>
-    props.highlights.includes(props.id)
-      ? "0 0 6px red"
-      : "0 0 6px rgba(0, 0, 0, 0.7)"};
+    props.highlights.includes(props.id) ? "0 0 6px red" : "none"};
 
   > div.rootcontent {
-    font-size: 0.6em;
+    font-size: 1em;
     font-weight: 700;
-    margin: 1em;
+    padding: 1em;
+    //margin: 0.6em;
+    color: white;
     word-break: keep-all;
   }
 `;
 
-const Nodebox = styled.div.attrs(({ id, coordX, coordY, highlights }) => {
-  return {
-    style: {
-      position: "fixed",
-      left: coordX + "px",
-      top: coordY + "px",
-      boxShadow: highlights.includes(id) ? "0 0 6px red" : "none",
-    },
-  };
-})`
+const Nodebox = styled.div.attrs(
+  ({ id, coordX, coordY, depth, highlights }) => {
+    return {
+      style: {
+        position: "fixed",
+        left: coordX + "px",
+        top: coordY + "px",
+        boxShadow: highlights.includes(id) ? "0 0 6px red" : "none",
+        filter: `hue-rotate(${String(depth * 20)}deg)`,
+        //opacity: depth * 10 + "%",
+      },
+    };
+  }
+)`
   z-index: 900;
-  font-size: 0.5em;
-  padding: 1em;
-  border-radius: 2em;
-  background-color: white;
+  font-size: 0.8em;
+  padding: 0.5em;
+  border-radius: 3em;
+  background-color: Aqua;
+  // theme 시작 컬러
   transform: translate(-50%, -50%);
   text-align: center;
+  font-weight: bold;
   border: solid grey 1px;
 
   > div.small {
     display: block;
+    padding: 1em;
   }
 
   > div.large {
@@ -180,6 +189,7 @@ const Nodebox = styled.div.attrs(({ id, coordX, coordY, highlights }) => {
     }
 
     > div.large {
+      padding: 0.8em;
       display: block;
       inline-size: 8em;
       line-height: 1.5em;
@@ -210,17 +220,19 @@ const Nodebox = styled.div.attrs(({ id, coordX, coordY, highlights }) => {
 const Lines = styled.svg`
   width: 100vw;
   height: 100vh;
+  stroke: rgb(100, 100, 100);
   overflow: visible;
+`;
 
-  > line {
-    stroke: rgb(180, 180, 180);
-    stroke-width: 1px;
-  }
-  //stroke-dasharray: 500;
-  stroke-dashoffset: 0;
+const Line = styled.line`
+  stroke-width: ${(props) => props.height + 2 + "px"};
+  opacity: ${(props) => (props.depth ? (props.depth + 2) * 20 + "%" : "50%")};
+
+  stroke-dasharray: 500;
+  stroke-dashoffset: 500;
   animation: dashDraw 1s linear 1;
-  // props.depth * delay(1s)
-  // useEffect ?
+  animation-fill-mode: forwards;
+  animation-delay: ${(props) => props.depth * 0.5 + "s"};
 
   @keyframes dashDraw {
     0% {
@@ -262,7 +274,7 @@ export default function Canvas3({
   ]);
 
   const [mapForm, setMapForm] = useState("cluster");
-  const [mapScale, setMapScale] = useState(2); // 마인드맵의 지름 비율
+  const [mapScale, setMapScale] = useState(3); // 마인드맵의 지름 비율
   const [highlight, setHighlight] = useState({ list: [], word: "" }); // 검색 시 하이라이트
   const [disabled, setDisabled] = useState(false); // 캔버스에서의 마우스 액션 허용/금지
   const [adjustScale, setAdjustScale] = useState(false); // 스케일 조절
@@ -324,6 +336,8 @@ export default function Canvas3({
     return {
       source: transformer(path.source.x, path.source.y),
       target: transformer(path.target.x, path.target.y),
+      depth: path.source.depth,
+      height: path.source.height,
     };
   });
 
@@ -331,7 +345,7 @@ export default function Canvas3({
   // 기본적인 scale 모델입니다. 비율은 추후 ui에 따라 변경가능 -> 이제 mapScale 로 자동조정됩니다.
 
   //console.log("radialNodes: ", radialNodes);
-  //console.log("radialLinkes: ", radialLinkes);
+  console.log("radialLinkes: ", radialLinkes);
   //console.log("links: ", links);
 
   const blockHandler = () => {
@@ -451,7 +465,7 @@ export default function Canvas3({
               <i className="fa-solid fa-magnifying-glass-minus"></i>
             </button>
             <button
-              onClick={() => centerView(1 / mapScale, 300, "easeOut")} // radialNodes[0].height ? mapScale
+              onClick={() => centerView(2 / mapScale, 300, "easeOut")} // radialNodes[0].height ? mapScale
               onMouseEnter={(e) => pilotHandler(e, "in", "중앙 정렬")}
               onMouseLeave={(e) => pilotHandler(e, "out")}
             >
@@ -497,7 +511,7 @@ export default function Canvas3({
             {adjustScale ? (
               <div>
                 <Scaler>
-                  <ul class="range-labels">
+                  <ul className="range-labels">
                     <li>-</li>
                     <li>+</li>
                   </ul>
@@ -532,7 +546,6 @@ export default function Canvas3({
           />
           <TransformComponent>
             {/* <Container> */}
-
             <Rootbox
               className="rootbox"
               id={radialNodes[0].data.id}
@@ -558,6 +571,7 @@ export default function Canvas3({
                 id={node.data.id}
                 coordY={node.y}
                 coordX={node.x}
+                depth={node.depth}
                 highlights={highlight.list.map((node) => node.data.id)}
                 onClick={() => pathHandler(node.data.id)}
                 onDragOver={(e) => {
@@ -580,30 +594,24 @@ export default function Canvas3({
                   </div>
                   {node.data.content}
                 </div>
-
-                {/* <div
-                  className="delete"
-                  onClick={(e) => deleteMindmapHandler(e, node.data.id)}
-                >
-                  <i className="fa-solid fa-rectangle-xmark"></i>
-                </div> */}
               </Nodebox>
             ))}
 
             <Lines>
               {radialLinkes.map((link, i) => {
                 return (
-                  <line
+                  <Line
                     key={i}
                     x1={link.source.x}
                     y1={link.source.y}
                     x2={link.target.x}
                     y2={link.target.y}
+                    depth={link.depth}
+                    height={link.height}
                   />
                 );
               })}
             </Lines>
-
             {/* </Container> */}
           </TransformComponent>
         </>
