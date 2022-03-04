ea1f84eb91bc965fec4581e68ee1d4b50fec1962
portis082 <portis082@gmail.com>
2022-03-04 13:43:41 +0900

    Update adder button css

--- luca-client/src/components/cardboard.js
+++ luca-client/src/components/cardboard.js
@@ -153,13 +153,6 @@ const Card = styled.div`
   filter: ${(props) => (props.blocked ? "brightness(50%)" : "none")};
   box-shadow: 0vh 0.5vh 1vh 0vh rgba(0, 0, 0, 0.3);
   > div.content {
-    margin: -1.5vh 1.5vh;
-    font-size: 2vh;
-    text-align: left;
-    word-break: break-word;
-    overflow: hidden;
-  }
-  > div.delete {
     position: relative;
     font-size: 1.6vh;
     line-height: 2vh;
@@ -202,17 +195,25 @@ const CardAdder = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  cursor: pointer;
   font-size: 5vh;
-  > i {
-    opacity: 30%;
+
+  > div.adderbox {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    cursor: pointer;
+    > i {
+      opacity: 30%;
+    }
   }
+
   > input {
     font-size: 0.7em;
     background: ${(props) => `rgb${props.color}`};
-    margin: auto;
+    padding: 0 10% 20% 10%;
     width: 80%;
-    height: 70%;
+    height: 60%;
     outline: none;
     border: none;
   }
@@ -228,6 +229,11 @@ const CardAdder = styled.div`
     font-weight: bold;
   }
   > div {
+    height: 20%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
     > button.yellow {
       background: rgb(253, 251, 209);
       margin: 5px;
@@ -381,8 +387,7 @@ export default function Cardboard({
   };
 
   const cardDragStart = (e) => {
-    // const img = new Image();
-    // e.dataTransfer.setDragImage(img, "7vh", "7vh");
+    e.dataTransfer.setDragImage(e.target, e.target.offsetHeight/2, e.target.offsetWidth/2);
 
     setDragItemId(e.target.id);
     // setIsAdderOpen(false)
@@ -473,6 +478,7 @@ export default function Cardboard({
           isCardContOpen={isCardContOpen}
           isAdderOpen={isAdderOpen}
           ref={outSection}
+
         >
           {isAdderOpen ? (
             <>
@@ -513,10 +519,11 @@ export default function Cardboard({
               </button> */}
             </>
           ) : (
-            <i
-              className="fa-solid fa-circle-plus"
-              onClick={adderOpenHandler}
-            ></i>
+            <div className="adderbox" onClick={adderOpenHandler}>
+              <i
+                className="fa-solid fa-circle-plus"
+              ></i>
+            </div>
           )}
         </CardAdder>
       </CardContainer>
