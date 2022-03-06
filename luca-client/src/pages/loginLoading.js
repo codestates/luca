549e54cce7038157da658cd23d94e63a9a3f3cec
AJ <86043065+WonjaeJi0801@users.noreply.github.com>
2022-03-07 08:27:35 +0900

    Add loading gifs

--- luca-client/src/pages/loginLoading.js
+++ luca-client/src/pages/loginLoading.js
@@ -4,33 +4,31 @@ import { useSelector } from "react-redux";
 import styled from "styled-components";
 
 const LoadingPage = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
 `;
 
 const LoginLoading = () => {
-	const Social = useSelector((state) => state.user.userInfo.isSocial);
-	const navigate = useNavigate();
+  const Social = useSelector((state) => state.user.userInfo.isSocial);
+  const navigate = useNavigate();
 
-	if (!Social) {
-		setTimeout(() => {
-			navigate("main");
-		}, 1500);
-		return (
-			<LoadingPage>
-				<img src="loginLoading.gif" alt="loading"/>
-			</LoadingPage>
-		);
-	} else {
-		setTimeout(() => {
-			navigate("main");
-		});
-		return (
-			null
-		);
-	}
+  if (!Social) {
+    setTimeout(() => {
+      navigate("main");
+    }, 2000);
+    return (
+      <LoadingPage>
+        <img src="loading_cards.gif" alt="loading" />
+      </LoadingPage>
+    );
+  } else {
+    setTimeout(() => {
+      navigate("main");
+    });
+    return null;
+  }
 };
 
 export default LoginLoading;
